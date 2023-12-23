1a535ed51a58b5fe67e65928dca59ce3c9034435
jagannath <jagannath.us.12@gmail.com>
2023-12-23 04:05:40 -0500

    add mutation logic

--- src/main/java/com/jagan/graphql/demo/controller/EmployeeGraphqlQuery.java
+++ src/main/java/com/jagan/graphql/demo/controller/EmployeeGraphqlQuery.java
@@ -1,8 +1,11 @@
 package com.jagan.graphql.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.jagan.graphql.demo.model.Employee;
 import com.jagan.graphql.demo.repository.EmployeeRepository;
@@ -17,5 +20,10 @@ public class EmployeeGraphqlQuery {
     @QueryMapping
     public Iterable<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    @MutationMapping
+    public Employee addEmployee(@Argument Employee employee){
+        return employeeRepository.save(employee);
     }
 }
